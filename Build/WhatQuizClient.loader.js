c6374057cabfe9a6c98bcb97210fb7f1953d7841
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-23 11:20:03 +0900

    플레이어 강퇴 대응

--- Build/WhatQuizClient.loader.js
+++ Build/WhatQuizClient.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95196707,
+    wasmFileSize: 95260786,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
