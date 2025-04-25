cd3ec53d6b3ee40378685bf5275a1d2942691d35
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-25 15:09:11 +0900

    데이터베이스 구조 개선 1차 테스트

--- Build/WhatQuizClient.loader.js
+++ Build/WhatQuizClient.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95260277,
+    wasmFileSize: 98817185,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
