d1ac33923c12afb7428b63feb654df0a9dc3bd3d
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-29 17:32:21 +0900

    2차 재작업

--- Build/WhatQuizClient.framework.js
+++ Build/WhatQuizClient.framework.js
@@ -1283,10 +1283,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7780592: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7780647: ($0) => { performance.now = function() { return $0; }; },  
- 7780695: ($0) => { performance.now = function() { return $0; }; },  
- 7780743: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7780720: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7780775: ($0) => { performance.now = function() { return $0; }; },  
+ 7780823: ($0) => { performance.now = function() { return $0; }; },  
+ 7780871: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
