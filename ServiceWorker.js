a9bfe40ab322e1a3528d0816537bcc8609c9d469
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-30 18:26:47 +0900

    2차 완성

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-WhatQuizSDK-1.0";
 const contentToCache = [
-    "Build/WhatQuizClient.loader.js",
-    "Build/WhatQuizClient.framework.js",
-    "Build/WhatQuizClient.data",
-    "Build/WhatQuizClient.wasm",
+    "Build/62345f78c071709a256797db892b09b9.loader.js",
+    "Build/29caab9275bf2a768d82142e73b67dfd.framework.js",
+    "Build/64c9806c71ad7c1f0b25b73de17a2240.data",
+    "Build/d4be55b95490973422a5afb592e75351.wasm",
     "TemplateData/style.css"
 
 ];
