78df90012c4c39a4f0c68aedb99dbe21d2de6829
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-30 15:11:53 +0900

    1차완성

--- Build/WhatQuizClient.loader.js
+++ Build/WhatQuizClient.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 97683265,
+    wasmFileSize: 97683379,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
