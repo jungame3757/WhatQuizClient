4be6b900454ef1d21c39a7b119ab7fae812e4d1b
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-30 16:26:14 +0900

    90%완성

--- Build/WhatQuizClient.framework.js
+++ Build/WhatQuizClient.framework.js
@@ -1283,10 +1283,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7780656: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7780711: ($0) => { performance.now = function() { return $0; }; },  
- 7780759: ($0) => { performance.now = function() { return $0; }; },  
- 7780807: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7780944: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7780999: ($0) => { performance.now = function() { return $0; }; },  
+ 7781047: ($0) => { performance.now = function() { return $0; }; },  
+ 7781095: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
